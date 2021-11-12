2f8873262c48e2d0a79dfa5d15b2e14df218ef81
Jason Lim <54863122+LinuxPythonSQL@users.noreply.github.com>
2021-11-12 14:07:46 -0800

    Header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "./components/Particle"
+import Navbar from "./components/Navbar";
+import Header from "./components/Header"
+function App() {
+  return (
+    <>
+      <Particles />
+      <Navbar />
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Typewriter from 'typewriter-effect';
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Data Analyst and Business Analyst</h1>
+                    <div className="typed-text">                       
+                        <Typewriter
+                            options={{
+                                strings: ["Data Analysis", "Data Visualization", "Business Analyst"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </div>
+                    <a href="#" className="btn-main-offer">Contact Me</a> 
+            </div> 
+        </div>
+    )
+}
+
+export default Header;
